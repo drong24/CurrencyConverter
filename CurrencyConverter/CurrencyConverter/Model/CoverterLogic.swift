ad7088d35730f5ba1591106ed0268e195eb20d76
drong24 <daisyrong24@gmail.com>
2024-06-28 14:18:32 -0700

    added converterlogic and errorlabel

---
+++ CurrencyConverter/CurrencyConverter/Model/CoverterLogic.swift
@@ -0,0 +1,14 @@
+//
+//  CoverterLogic.swift
+//  CurrencyConverter
+//
+//  Created by Daisy Rong on 6/28/24.
+//
+
+import Foundation
+
+struct ConverterLogic {
+    func convertCurrency(amount: Double, convertOne: Bool, convertTwo: Bool, convertThree: Bool, convertFour: Bool) {
+        
+    }
+}
--- CurrencyConverter/CurrencyConverter/ViewController.swift
+++ CurrencyConverter/CurrencyConverter/ViewController.swift
@@ -9,11 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var InputAmount: UITextField!
+    
+    @IBOutlet weak var SwitchOne: UISwitch!
+    
+    @IBOutlet weak var SwitchTwo: UISwitch!
+    
+    @IBOutlet weak var SwitchThree: UISwitch!
+    
+    @IBOutlet weak var SwitchFour: UISwitch!
+    
+    @IBOutlet weak var ErrorLabel: UILabel!
+    
+    var converterLogic = ConverterLogic()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func ConvertCurrencyButton(_ sender: UIButton) {
+        //check if input is valid
+        if (InputAmount.text == "") {
+            ErrorLabel.text = ""
+            converterLogic.convertCurrency(amount: 44.4, convertOne: SwitchOne.isOn, convertTwo: SwitchTwo.isOn, convertThree: SwitchOne.isOn, convertFour: SwitchOne.isOn)
+        }
+        else {
+            ErrorLabel.text = "Please enter a valid integer"
+        }
 
+    }
+    
 }
 
